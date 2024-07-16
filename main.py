7fdfb8a038e26b1070ef87d288c749eb3dff4e95
UsamaBashir007009 <usama.bashir@arbisoft.com>
2024-07-16 20:07:33 +0500

    added: methods

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import random
+
+
 def print_statement(text):
     print(text)
+    
+def get_random():
+    return random.randint(0, 11)
 
 
 def get_name():
     return "Usama Bashir"
 
-
-name = get_name()
-print_statement(name)
+if __name__ == '__main__':
+    name = get_name()
+    print_statement(name)
+    print(get_random())

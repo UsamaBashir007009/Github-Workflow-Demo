da1fa9aad8ab66975c7a03119bc0f81cdaed21b8
UsamaBashir007009 <usama.bashir@arbisoft.com>
2024-07-16 21:42:47 +0500

    added: tests

--- test/test_cal.py
+++ test/test_cal.py
@@ -1,7 +1,11 @@
-from cal import add
+from cal import add, subtract
 
 
 def test_add():
     assert add(1, 7) == 8
     assert add(1, 1) == 2
     assert add(1, -7) == -6
+
+def test_subtract():
+    assert subtract(1, 7) == -6
+    assert subtract(1, 9) == -8
